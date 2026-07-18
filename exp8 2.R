637bd2e94b7606462d51bcefbe16932e23573e9a
Sai Chetan Yanduru <yanduruchetan@gmail.com>
2026-07-18 15:43:07 +0530

    Delete exp8 2.R

--- exp8 2.R
+++
@@ -1,6 +0,0 @@
-dept_count <- table(department)
-barplot(dept_count,
-        col="lightgreen",
-        main="Employees by Department",
-        xlab="Department",
-        ylab="Number of Employees")
